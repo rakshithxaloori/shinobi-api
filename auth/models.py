12f965d710e996e986cb222a3028dc905b82827e
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-06-18 12:30:34 +0530

    add is_banned field to AbstractUser

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class User(AbstractBaseUser):
+    is_banned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} || {}".format(self.username, self.email)
