73c6445616cdc43ae7d7987294eb07671edb1781
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-07-05 15:08:01 +0530

    cascade msgs on chat delete

--- chat/models.py
+++ chat/models.py
@@ -16,7 +16,7 @@ class Chat(models.Model):
 
 
 class Message(models.Model):
-    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.PROTECT)
+    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.CASCADE)
     sent_by = models.ForeignKey(
         User, related_name="sent_messages", on_delete=models.PROTECT
     )
