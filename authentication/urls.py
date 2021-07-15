7836885b05539721c91162df6ecd4dd9fa48fc38
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-07-15 22:17:02 +0530

    logout url

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,6 +5,7 @@ from authentication import views
 urlpatterns = [
     path("login/google/", views.google_login_view, name="google login"),
     path("signup/google/", views.google_signup_view, name="google signup"),
+    path("logout/", views.logout_view, name="logout"),
     path("check_username/", views.check_username_view, name="check username"),
     path("token_valid/", views.token_valid_view, name="token valid"),
 ]
