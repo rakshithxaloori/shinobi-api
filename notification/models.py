380d06a9fdf1d9509bc5f902e3625894e0df5c45
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-07-22 12:09:05 +0530

    model init

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Notification(models.Model):
+    FOLLOW = "f"
+
+    NOTIFICATION_CHOICES = [(FOLLOW, "follow")]
+
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    type = models.CharField(
+        max_length=1,
+        choices=NOTIFICATION_CHOICES,
+        default=FOLLOW,
+        blank=False,
+        null=False,
+    )
+    sent_at = models.DateTimeField(default=timezone.now)
