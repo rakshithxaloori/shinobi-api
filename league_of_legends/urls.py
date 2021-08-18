92363e9f44ae2f84576479db5a15b77902532b01
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-08-18 16:08:48 +0530

    match history

--- league_of_legends/urls.py
+++ league_of_legends/urls.py
@@ -5,7 +5,17 @@ from league_of_legends import views
 urlpatterns = [
     path("my_profile/", views.my_lol_profile_view, name="my lol profile"),
     path(
-        "champion_masteries/<str:username>/<int:beginIndex>/<int:endIndex>/",
+        "matches/<str:username>/<int:match_index>/",
+        views.match_history_view,
+        name="match history",
+    ),
+    # path(
+    #     "matches/<str:username>/<int:begin_index>/<int:end_index>/",
+    #     views.match_history_view,
+    #     name="match history",
+    # ),
+    path(
+        "masteries/<str:username>/<int:begin_index>/<int:end_index>/",
         views.champion_masteries_view,
         name="champion masteries",
     ),
--- league_of_legends/views.py
+++ league_of_legends/views.py
@@ -1,3 +1,4 @@
+from cassiopeia.core import summoner
 from django.http import JsonResponse
 
 from rest_framework import status
@@ -19,7 +20,7 @@ from django_cassiopeia import cassiopeia as cass
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated, HasAPIKey])
 def my_lol_profile_view(request):
-    summoner = cass.get_summoner(name="Kalturi", region="NA")
+    summoner = cass.get_summoner(name="bigfatlp", region="NA")
     return JsonResponse(
         {
             "detail": "{}'s lol profile".format(request.user.username),
@@ -37,7 +38,7 @@ def my_lol_profile_view(request):
 @api_view(["GET"])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated, HasAPIKey])
-def champion_masteries_view(request, username=None, beginIndex=0, endIndex=20):
+def match_history_view(request, username=None, match_index=0):
     if username is None:
         return JsonResponse(
             {"detail": "username is required"}, status=status.HTTP_400_BAD_REQUEST
@@ -50,8 +51,179 @@ def champion_masteries_view(request, username=None, beginIndex=0, endIndex=20):
     # except (User.DoesNotExist, LeagueOfLegendsProfile.DoesNotExist):
     #     return JsonResponse({"detail": ""}, status=status.HTTP_404_NOT_FOUND)
 
-    summoner = cass.get_summoner(name="Kalturi", region="NA")
-    champion_masteries = summoner.champion_masteries[beginIndex:endIndex]
+    summoner = cass.get_summoner(name="bigfatlp")
+    try:
+        match = summoner.match_history[match_index]
+        match_dict = {}
+        match_dict["id"] = match.id
+        match_dict["creation"] = match.creation.for_json()
+        team = None
+        participant = None
+
+        for p in match.blue_team.participants:
+            if summoner == p.summoner:
+                team = match.blue_team
+                participant = p
+                break
+
+        if team is None:
+            for p in match.red_team.participants:
+                if summoner == p.summoner:
+                    team = match.red_team
+                    participant = p
+                    break
+
+        match_dict["team"] = {"side": team.side.name, "win": team.win}
+
+        match_dict["participant"] = {
+            "role": participant.role.value,
+            "champion": {
+                "name": participant.champion.name,
+                "image": participant.champion.image.url,
+                "key": participant.champion.key,
+            },
+            "stats": {
+                "assists": participant.stats.assists,
+                "deaths": participant.stats.deaths,
+                "kills": participant.stats.kills,
+            },
+        }
+        items = []
+
+        for item in participant.stats.items:
+            if item is not None:
+                items.append(
+                    {
+                        "name": item.name,
+                        "image": "http://ddragon.leagueoflegends.com/cdn/11.16.1/img/item/{}".format(
+                            item.image.full
+                        ),
+                    }
+                )
+
+        match_dict["participant"]["stats"]["items"] = items
+
+        return JsonResponse(
+            {
+                "detail": "{}'s match history".format(summoner.name),
+                "match": match_dict,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    except Exception as e:
+        print(e)
+        return JsonResponse(
+            {"detail": "Match doesn't exist"}, status=status.HTTP_404_NOT_FOUND
+        )
+
+
+# @api_view(["GET"])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated, HasAPIKey])
+# def match_history_view(request, username=None, begin_index=0, end_index=10):
+#     if username is None:
+#         return JsonResponse(
+#             {"detail": "username is required"}, status=status.HTTP_400_BAD_REQUEST
+#         )
+
+#     # try:
+#     #     summoner_name = User.objects.get(
+#     #         username=username
+#     #     ).league_of_legends_profile_name
+#     # except (User.DoesNotExist, LeagueOfLegendsProfile.DoesNotExist):
+#     #     return JsonResponse({"detail": ""}, status=status.HTTP_404_NOT_FOUND)
+
+#     if begin_index > end_index:
+#         return JsonResponse(
+#             {"detail": "Bad indices"}, status=status.HTTP_400_BAD_REQUEST
+#         )
+
+#     summoner = cass.get_summoner(name="bigfatlp")
+#     match_history = []
+#     for match in summoner.match_history[begin_index:end_index]:
+#         match_dict = {}
+#         match_dict["creation"] = match.creation.for_json()
+#         team = None
+#         participant = None
+
+#         for p in match.blue_team.participants:
+#             if summoner == p.summoner:
+#                 team = match.blue_team
+#                 participant = p
+#                 break
+
+#         if team is None:
+#             for p in match.red_team.participants:
+#                 if summoner == p.summoner:
+#                     team = match.red_team
+#                     participant = p
+#                     break
+
+#         match_dict["team"] = {"side": team.side.name, "win": team.win}
+
+#         match_dict["participant"] = {
+#             "role": participant.role.value,
+#             "champion": {
+#                 "name": participant.champion.name,
+#                 "image": participant.champion.image.url,
+#                 "key": participant.champion.key,
+#             },
+#             "stats": {
+#                 "assists": participant.stats.assists,
+#                 "deaths": participant.stats.deaths,
+#                 "kills": participant.stats.kills,
+#             },
+#         }
+#         items = []
+
+#         for item in participant.stats.items:
+#             if item is not None:
+#                 items.append(
+#                     {
+#                         "name": item.name,
+#                         "image": "http://ddragon.leagueoflegends.com/cdn/11.16.1/img/item/{}".format(
+#                             item.image.full
+#                         ),
+#                     }
+#                 )
+
+#         match_dict["participant"]["stats"]["items"] = items
+
+#         match_history.append(match_dict)
+
+#     return JsonResponse(
+#         {
+#             "detail": "{}'s match history".format(summoner.name),
+#             "match_history": match_history,
+#         },
+#         status=status.HTTP_200_OK,
+#     )
+
+
+@api_view(["GET"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated, HasAPIKey])
+def champion_masteries_view(request, username=None, begin_index=0, end_index=20):
+    if username is None:
+        return JsonResponse(
+            {"detail": "username is required"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # try:
+    #     summoner_name = User.objects.get(
+    #         username=username
+    #     ).league_of_legends_profile_name
+    # except (User.DoesNotExist, LeagueOfLegendsProfile.DoesNotExist):
+    #     return JsonResponse({"detail": ""}, status=status.HTTP_404_NOT_FOUND)
+
+    if begin_index > end_index:
+        return JsonResponse(
+            {"detail": "Bad indices"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    summoner = cass.get_summoner(name="bigfatlp", region="NA")
+    champion_masteries = summoner.champion_masteries[begin_index:end_index]
     # champion_masteries = summoner.champion_masteries
     champion_mastery_by_level = dict()
 
