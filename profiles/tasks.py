96f17c6d5aba4145fd33e8d912cc8fd7d960ce93
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-09-03 20:27:45 +0530

    follower count field

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -3,8 +3,6 @@ from celery import shared_task
 from authentication.models import User
 from profiles.models import Profile
 
-trend_score = 1
-
 
 @shared_task
 def add_profile_picture(user_pk, picture):
@@ -24,8 +22,8 @@ def after_follow(user_profile_pk):
         user_profile = Profile.objects.get(pk=user_profile_pk)
     except Profile.DoesNotExist:
         return
-    user_profile.trend_score += trend_score
-    user_profile.save(update_fields=["trend_score"])
+    user_profile.follower_count = user_profile.user.follower.count()
+    user_profile.save(update_fields=["follower_count"])
 
 
 @shared_task
@@ -34,5 +32,5 @@ def after_unfollow(user_profile_pk):
         user_profile = Profile.objects.get(pk=user_profile_pk)
     except Profile.DoesNotExist:
         return
-    user_profile.trend_score -= trend_score
-    user_profile.save(update_fields=["trend_score"])
+    user_profile.follower_count = user_profile.user.follower.count()
+    user_profile.save(update_fields=["follower_count"])
