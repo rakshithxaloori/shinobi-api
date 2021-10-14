11d51880c3bd0e30f0fcffba2a117c80948cbb29
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-10-14 23:42:18 +0530

    fix name bugs

--- .platform/hooks/prebuild/01_login.sh
+++ .platform/hooks/prebuild/01_login.sh
@@ -1,5 +1,5 @@
 ### 01login.sh
 #!/bin/bash
-USER=/opt/elasticbeanstalk/bin/get-config environment -k ECR_USER
-PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k ECR_PASSWD
+ECR_USER=/opt/elasticbeanstalk/bin/get-config environment -k ECR_USER
+ECR_PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k ECR_PASSWD
 docker login -u $ECR_USER -p $ECR_PASSWD
