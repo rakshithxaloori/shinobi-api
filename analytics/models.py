f25b4a3627ed6358230a58a26cf74dd88f919b68
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-11-24 23:38:22 +0530

    add total_clips analytics

--- analytics/models.py
+++ analytics/models.py
@@ -5,9 +5,10 @@ from shinobi.utils import now_date
 
 class DailyAnalytics(models.Model):
     date = models.DateField(primary_key=True, default=now_date)
-    new_users = models.PositiveBigIntegerField(default=0)
-    active_users = models.PositiveBigIntegerField(default=0)
-    total_users = models.PositiveBigIntegerField(default=0)
+    new_users = models.PositiveIntegerField(default=0)
+    active_users = models.PositiveIntegerField(default=0)
+    total_users = models.PositiveIntegerField(default=0)
+    total_clips = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return "{} || a: {} || n: {} || t: {} || c: {}".format(
@@ -15,5 +16,5 @@ class DailyAnalytics(models.Model):
             self.active_users,
             self.new_users,
             self.total_users,
-            self.clips_uploaded,
+            self.total_clips,
         )
