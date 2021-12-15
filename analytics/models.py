d1c22e62868f796e78d11d5674b59a2ad1b0d2ac
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-12-15 10:15:32 +0530

    add views to analytics str

--- analytics/models.py
+++ analytics/models.py
@@ -13,11 +13,12 @@ class DailyAnalytics(models.Model):
     total_views = models.PositiveBigIntegerField(default=0)
 
     def __str__(self):
-        return "{} || a: {} || n: {} || t: {} || cm: {} || cw: {}".format(
+        return "{} || a: {} || n: {} || t: {} || cm: {} || cw: {} || v: {}".format(
             self.date,
             self.active_users,
             self.new_users,
             self.total_users,
             self.total_clips_m,
             self.total_clips_w,
+            self.total_views,
         )
