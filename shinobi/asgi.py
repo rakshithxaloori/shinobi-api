b733301213a1914cb8f37c028ffefdee2e2a0b30
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-01-14 16:27:34 +0530

    fix asgi application error

--- shinobi/asgi.py
+++ shinobi/asgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shinobi.settings")
 
 django.setup()
 
-application = get_default_application()
+application = get_asgi_application()
