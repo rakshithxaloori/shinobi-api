dd442ee6cbcbe5ea8dcbc2374ea31b79933d4e58
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-02-01 12:16:29 +0530

    weekly, monthy analytics init

--- analytics/models.py
+++ analytics/models.py
@@ -22,3 +22,45 @@ class DailyAnalytics(models.Model):
             self.total_clips_w,
             self.total_views,
         )
+
+
+class WeeklyAnalytics(models.Model):
+    date = models.DateField(primary_key=True, default=now_date)
+    new_users = models.PositiveIntegerField(default=0)
+    active_users = models.PositiveIntegerField(default=0)
+    total_users = models.PositiveIntegerField(default=0)
+    total_clips_m = models.PositiveIntegerField(default=0)
+    total_clips_w = models.PositiveIntegerField(default=0)
+    total_views = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return "{} || a: {} || n: {} || t: {} || cm: {} || cw: {} || v: {}".format(
+            self.date,
+            self.active_users,
+            self.new_users,
+            self.total_users,
+            self.total_clips_m,
+            self.total_clips_w,
+            self.total_views,
+        )
+
+
+class MonthlyAnalytics(models.Model):
+    date = models.DateField(primary_key=True, default=now_date)
+    new_users = models.PositiveIntegerField(default=0)
+    active_users = models.PositiveIntegerField(default=0)
+    total_users = models.PositiveIntegerField(default=0)
+    total_clips_m = models.PositiveIntegerField(default=0)
+    total_clips_w = models.PositiveIntegerField(default=0)
+    total_views = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return "{} || a: {} || n: {} || t: {} || cm: {} || cw: {} || v: {}".format(
+            self.date,
+            self.active_users,
+            self.new_users,
+            self.total_users,
+            self.total_clips_m,
+            self.total_clips_w,
+            self.total_views,
+        )
--- analytics/tasks.py
+++ analytics/tasks.py
@@ -4,17 +4,40 @@ from celery.schedules import crontab
 from shinobi.celery import app as celery_app
 
 from authentication.models import User
-from clips.models import Clip, View
-from analytics.models import DailyAnalytics
+from clips.models import Clip
+from analytics.models import DailyAnalytics, MonthlyAnalytics, WeeklyAnalytics
 from shinobi.utils import now_date
 
 
 def get_da_instance():
     try:
-        analytics = DailyAnalytics.objects.get(date=now_date())
+        analytics = DailyAnalytics.objects.order_by("-date").first()
 
-    except DailyAnalytics.DoesNotExist:
-        analytics = DailyAnalytics.objects.get(pk=create_new_da())
+    except (AttributeError, DailyAnalytics.DoesNotExist):
+        pk = create_new_da()
+        analytics = DailyAnalytics.objects.get(pk=pk)
+
+    return analytics
+
+
+def get_wa_instance():
+    try:
+        analytics = WeeklyAnalytics.objects.order_by("-date").first()
+
+    except (AttributeError, WeeklyAnalytics.DoesNotExist):
+        pk = create_new_wa()
+        analytics = WeeklyAnalytics.objects.get(pk=pk)
+
+    return analytics
+
+
+def get_ma_instance():
+    try:
+        analytics = MonthlyAnalytics.objects.order_by("-date").first()
+
+    except (AttributeError, MonthlyAnalytics.DoesNotExist):
+        pk = create_new_ma()
+        analytics = MonthlyAnalytics.objects.get(pk=pk)
 
     return analytics
 
@@ -24,6 +47,77 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         crontab(minute=0, hour=0), create_new_da.s(), name="create daily analytics"
     )
+
+    sender.add_periodic_task(
+        crontab(minute=0, hour=0, day_of_week="mon"),
+        create_new_wa.s(),
+        name="create weekly analytics",
+    )
+
+    sender.add_periodic_task(
+        crontab(minute=0, hour=0, day_of_week="mon"),
+        check_new_ma.s(),
+        name="create monthly analytics",
+    )
+
+
+@celery_app.task(queue="celery")
+def update_active_user_task(last_open_str):
+    last_open_date = timezone.datetime.fromisoformat(last_open_str).date()
+
+    ## Daily Analytics
+    da_instance = get_da_instance()
+    if last_open_date < da_instance.date:
+        # If last opened before today
+        da_instance.active_users += 1
+        da_instance.save(update_fields=["active_users"])
+
+    ## Weekly Analytics
+    wa_instance = get_wa_instance()
+    if last_open_date < wa_instance.date:
+        # If last opened before this week
+        wa_instance.active_users += 1
+        wa_instance.save(update_fields=["active_users"])
+
+    ## Monthly Analytics
+    ma_instance = get_ma_instance()
+    if last_open_date < ma_instance.date:
+        # If last opened before this month
+        ma_instance.active_users += 1
+        ma_instance.save(update_fields=["active_users"])
+
+
+@celery_app.task(queue="celery")
+def new_user_joined_task():
+    da = get_da_instance()
+    da.active_users += 1
+    da.new_users += 1
+    da.save(update_fields=["active_users", "new_users"])
+
+    wa = get_wa_instance()
+    wa.active_users += 1
+    wa.new_users += 1
+    wa.save(update_fields=["active_users", "new_users"])
+
+    ma = get_ma_instance()
+    ma.active_users += 1
+    ma.new_users += 1
+    ma.save(update_fields=["active_users", "new_users"])
+
+
+@celery_app.task(queue="celery")
+def add_view_analytics_task():
+    da = get_da_instance()
+    da.total_views += 1
+    da.save(update_fields=["total_views"])
+
+    wa = get_wa_instance()
+    wa.total_views += 1
+    wa.save(update_fields=["total_views"])
+
+    ma = get_ma_instance()
+    ma.total_views += 1
+    ma.save(update_fields=["total_views"])
 
 
 @celery_app.task(queue="celery")
@@ -52,32 +146,55 @@ def create_new_da():
 
 
 @celery_app.task(queue="celery")
-def new_user_joined():
-    analytics = get_da_instance()
-    analytics.active_users += 1
-    analytics.new_users += 1
-    analytics.save(update_fields=["active_users", "new_users"])
+def create_new_wa():
+    old_analytics = WeeklyAnalytics.objects.order_by("-date").first()
+    old_analytics.total_users = User.objects.filter(is_staff=False).count()
+    old_analytics.total_clips_m = Clip.objects.filter(
+        created_date__gte=old_analytics.date, uploaded_from=Clip.MOBILE
+    ).count()
+    old_analytics.total_clips_w = Clip.objects.filter(
+        created_date__gte=old_analytics.date, uploaded_from=Clip.WEB
+    ).count()
+    old_analytics.save(
+        update_fields=[
+            "total_users",
+            "total_clips_m",
+            "total_clips_w",
+        ]
+    )
 
-
-@celery_app.task(queue="celery")
-def update_active_user(last_open_str):
-    last_open_date = timezone.datetime.fromisoformat(last_open_str).date()
-
-    # Daily Analytics
-    da_instance = get_da_instance()
-    if da_instance is None:
-        # Happens when the db is first created
-        da_instance = DailyAnalytics.objects.create(date=timezone.now())
-        da_instance.save()
-
-    elif last_open_date < da_instance.date:
-        # If last opened before today
-        da_instance.active_users += 1
-        da_instance.save(update_fields=["active_users"])
-
-
-@celery_app.task(queue="celery")
-def add_view_analytics_task():
-    analytics = get_da_instance()
-    analytics.total_views += 1
+    # Create new analytics instance
+    analytics = WeeklyAnalytics.objects.create()
     analytics.save()
+    return analytics.pk
+
+
+@celery_app.task(queue="celery")
+def check_new_ma():
+    old_analytics = MonthlyAnalytics.objects.order_by("-date").first()
+    if (now_date() - old_analytics.date).days >= 4 * 7:
+        create_new_ma()
+
+
+@celery_app.task(queue="celery")
+def create_new_ma():
+    old_analytics = MonthlyAnalytics.objects.order_by("-date").first()
+    old_analytics.total_users = User.objects.filter(is_staff=False).count()
+    old_analytics.total_clips_m = Clip.objects.filter(
+        created_date__gte=old_analytics.date, uploaded_from=Clip.MOBILE
+    ).count()
+    old_analytics.total_clips_w = Clip.objects.filter(
+        created_date__gte=old_analytics.date, uploaded_from=Clip.WEB
+    ).count()
+    old_analytics.save(
+        update_fields=[
+            "total_users",
+            "total_clips_m",
+            "total_clips_w",
+        ]
+    )
+
+    # Create new analytics instance
+    analytics = MonthlyAnalytics.objects.create()
+    analytics.save()
+    return analytics.pk
