96bef09a0bee2fa80c80d54937f05c34fdfae4a5
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-02-27 21:44:43 +0530

    check user exists with email

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("login/google/", views.google_login_view, name="google login"),
     path("signup/google/", views.google_signup_view, name="google signup"),
     path("logout/", views.logout_view, name="logout"),
+    path("check_email/", views.check_email_view, name="check email"),
     path("check_username/", views.check_username_view, name="check username"),
     path("online/", views.online_view, name="online"),
     path("offline/", views.offline_view, name="offline"),
